fd3688a51219738864cdd334cc3a0c9ad0b09253
paxan <paxan2323232@gmail.com>
2026-02-04 14:44:06 +0300

    feat: management command для обновления названия сайта

---
+++ landing/management/commands/update_site_name.py
@@ -0,0 +1,21 @@
+"""
+Management команда для обновления названия сайта
+"""
+from django.core.management.base import BaseCommand
+from landing.models import SiteSettings
+
+
+class Command(BaseCommand):
+    help = 'Обновление названия сайта: Птицелов → Арсенал'
+
+    def handle(self, *args, **options):
+        settings = SiteSettings.objects.first()
+        if settings:
+            settings.site_title = 'Арсенал'
+            settings.hero_title = 'АРСЕНАЛ'
+            settings.save()
+            self.stdout.write(self.style.SUCCESS(
+                f'✓ Обновлено: site_title={settings.site_title}, hero_title={settings.hero_title}'
+            ))
+        else:
+            self.stdout.write(self.style.WARNING('SiteSettings не найдены'))
